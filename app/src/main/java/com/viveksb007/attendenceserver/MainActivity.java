a457d0bdf584f794de92eebe5210e508d43c6d5a
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2016-09-14 19:51:24 +0530

    yet to test

--- app/src/main/java/com/viveksb007/attendenceserver/MainActivity.java
+++ app/src/main/java/com/viveksb007/attendenceserver/MainActivity.java
@@ -11,10 +11,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -28,9 +29,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final String TAG = getClass().getSimpleName();
     public WifiManager mWifiManager;
     Button startHotspot, stopHotspot;
-    TextView tvClientNum, tvIPinfo, socketInfo, clientInfo;
+    TextView tvIPinfo, socketInfo, clientInfo;
     ServerSocket serverSocket;
-    String msg = "";
+    String msg = "", messageFromClient = "";
+    Thread socketServerThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +40,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         startHotspot = (Button) findViewById(R.id.button);
         stopHotspot = (Button) findViewById(R.id.button2);
-        tvClientNum = (TextView) findViewById(R.id.textView);
         tvIPinfo = (TextView) findViewById(R.id.tvIPinfo);
         socketInfo = (TextView) findViewById(R.id.tvSocketInfo);
         clientInfo = (TextView) findViewById(R.id.tvClientInfo);
-
-        tvIPinfo.setText(getIpAddress());
 
         startHotspot.setOnClickListener(this);
         stopHotspot.setOnClickListener(this);
@@ -57,10 +56,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.button:
                 turnOnWifiHotspot();
+                startServer();
+                while (getIpAddress().equals("")){}
+                tvIPinfo.setText(getIpAddress());
                 break;
             case R.id.button2:
+                stopServer();
                 turnOffWifiHotspot();
+                tvIPinfo.setText("");
                 break;
+        }
+    }
+
+    private void stopServer() {
+        if (serverSocket != null) {
+            try {
+                socketServerThread.interrupt();
+                serverSocket.close();
+                Toast.makeText(MainActivity.this, "ServerSocket CLOSED", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -98,17 +114,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Method getWifiAPConfigMethod = mWifiManager.getClass().getMethod("getWifiApConfiguration");
             wifiConfiguration = (WifiConfiguration) getWifiAPConfigMethod.invoke(mWifiManager);
 
-            tvClientNum.setText(wifiConfiguration.SSID);
             Log.v("Client", "SSID\n" + wifiConfiguration.SSID + "\nPassword : " + wifiConfiguration.preSharedKey);
         } catch (Exception e) {
             Log.e(TAG, e.toString());
         }
 
-        startServer();
     }
 
     private void startServer() {
-        Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread = new Thread(new SocketServerThread());
         socketServerThread.start();
     }
 
@@ -117,6 +131,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onDestroy();
         if (serverSocket != null) {
             try {
+                socketServerThread.interrupt();
                 serverSocket.close();
                 Toast.makeText(MainActivity.this, "ServerSocket CLOSED", Toast.LENGTH_SHORT).show();
             } catch (IOException e) {
@@ -127,12 +142,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private class SocketServerThread extends Thread {
         static final int SOCKET_SERVER_PORT = 8080;
-        int count = 0;
-
         @Override
         public void run() {
+            Socket socket = null;
+            DataInputStream dataInputStream = null;
+            DataOutputStream dataOutputStream = null;
+            String macAddresses = "";
             try {
                 serverSocket = new ServerSocket(SOCKET_SERVER_PORT);
+
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -141,63 +159,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 });
 
                 while (true) {
-                    Socket socket = serverSocket.accept();
-                    count++;
-                    msg += "#" + count + " from " + socket.getInetAddress() + ":" + socket.getPort() + "\n";
+                    socket = serverSocket.accept();
+                    dataInputStream = new DataInputStream(socket.getInputStream());
+                    dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
+                    messageFromClient = dataInputStream.readUTF();
+                    macAddresses += messageFromClient;
+
+                    final String finalMacAddressess = macAddresses;
                     MainActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            clientInfo.setText(msg);
-                            Toast.makeText(getApplicationContext(), "Count:" + count, Toast.LENGTH_SHORT).show();
+                            clientInfo.setText(finalMacAddressess);
                         }
                     });
 
-                    SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(socket, count);
-                    socketServerReplyThread.run();
-
+                    if(messageFromClient == null){
+                        dataOutputStream.writeUTF("404");
+                    }else{
+                        dataOutputStream.writeUTF("200");
+                    }
                 }
 
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-        }
-    }
+            } finally {
 
-    private class SocketServerReplyThread extends Thread {
-
-        private Socket hostThreadSocket;
-        int count;
-
-        SocketServerReplyThread(Socket socket, int c) {
-            hostThreadSocket = socket;
-            count = c;
-        }
-
-        @Override
-        public void run() {
-            OutputStream outputStream;
-            String msgReply = "Hello from AttendenceServer, you are @" + count;
-            try {
-                outputStream = hostThreadSocket.getOutputStream();
-                PrintStream printStream = new PrintStream(outputStream);
-                printStream.print(msgReply);
-                printStream.close();
-
-                hostThreadSocket.close();
-
-                msg += "Replied : " + msgReply + "\n";
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        clientInfo.setText(msg);
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                });
-            } catch (IOException e) {
-                e.printStackTrace();
+                }
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }
         }
     }
+
 
     private String getIpAddress() {
         String ip = "";
@@ -222,7 +236,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
 
         } catch (SocketException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
             ip += "Something Wrong! " + e.toString() + "\n";
         }
